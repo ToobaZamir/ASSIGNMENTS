2ff678be8d26589b36334a5fb40f21098615f9d4
ToobaZamir <toobaahmed215@gmail.com>
2025-03-27 11:14:17 +0500

    converter.py

--- converter.py
+++ converter.py
@@ -62,7 +62,7 @@ def convert_units(category, value, unit):
 if category == "Length":
     unit = st.selectbox("📏 Select Converter", ["Kilometer to Metre","Metre to Kilometer","Metre to Centimetre","Centimetre to Milimetre","Inch to Foot","Foot to Mile","Centimetre to Inche","Inch to Centimetre"])
 elif category == "Mass":
-    unit = st.selectbox("⚖️ Select Converter", ["Kilogram to Gram","Gram to Milligram","Pound to Ounce","Pound to Miligram","Pound to Stone","Ounce to Kilogram","Gram to Microgram"])
+    unit = st.selectbox("⚖️ Select Converter", ["Kilogram to Gram","Gram to Milligram","Pound to Ounce","Pound to Milligram","Pound to Stone","Ounce to Kilogram","Gram to Microgram"])
 elif category == "Time":
     unit = st.selectbox("⏱️ Select Converter", ["Second to Minute","Minute to Hour","Minute to Day","Hour to Second","Hour to Day","Week to Hour","Day to Week"])
 
